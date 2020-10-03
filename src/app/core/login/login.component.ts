b9fd8f72538120b0ce0de89fe7a792fd5f73c460
akshayjadhavaj1 <jadhavakshayjj8@gmail.com>
2020-10-03 12:22:22 +0530

    Added remeber me functionality

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -18,7 +18,21 @@ export class LoginComponent implements OnInit {
   isInvalidUser: boolean;
   constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.keepSigned = false;
+    this.loginForm.user_login_id = '';
+    this.loginForm.user_login_password = '';
+    if (localStorage.getItem('rememberMe')) {
+      console.log(JSON.parse(localStorage.getItem('rememberMe')));
+      this.keepSigned = true;
+      this.loginForm.user_login_id = JSON.parse(
+        localStorage.getItem('rememberMe')
+      ).user_login_id;
+      this.loginForm.user_login_password = JSON.parse(
+        localStorage.getItem('rememberMe')
+      ).user_login_password;
+    } 
+  }
 
   login() {
     this.isInvalidUser = false;
@@ -30,10 +44,19 @@ export class LoginComponent implements OnInit {
       return;
     }
     if (this.authService.validateUser(this.loginForm)) {
-      localStorage.setItem('loginUser', this.loginForm.user_login_id);
+      localStorage.setItem('loginUser', JSON.stringify(this.loginForm));
       this.router.navigateByUrl('/home');
     } else {
       this.isInvalidUser = true;
+    }
+  }
+
+  rememeberMe($event) {
+    this.keepSigned = $event.target.checked;
+    if ($event.target.checked) {
+      localStorage.setItem('rememberMe', JSON.stringify(this.loginForm));
+    } else {
+      localStorage.removeItem('rememberMe');
     }
   }
 }
